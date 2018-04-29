6f6b90f95b46b4e1e4ffb1707176d745ea49f946
yatongzhao <yatongzhao@sohu-inc.com>
2018-04-29 11:04:45 +0800

    初步完成页面布局

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,32 @@
 <template>
   <div class="main">
-    <div class="player1">player1</div>
-    <div class="player2">player2</div>
+    <button class="player1">player1</button>
+    <button class="player2">player2</button>
+    <button class="end-round">结束回合</button>
+    <button class="ground"></button>
+    <div class="opponent-table">
+      <button class="card-in-table"></button>
+      <button class="card-in-table"></button>
+      <button class="card-in-table"></button>
+    </div>
+    <div class="self-table">
+      <button class="card-in-table"></button>
+      <button class="card-in-table"></button>
+      <button class="card-in-table"></button>
+      <button class="card-in-table"></button>
+      <button class="card-in-table"></button>
+      <button class="card-in-table"></button>
+    </div>
+    <div class="opponent-hand">
+      <button class="card-in-hand"></button>
+    </div>
+    <div class="self-hand">
+      <button class="card-in-hand"></button>
+      <button class="card-in-hand"></button>
+      <button class="card-in-hand"></button>
+      <button class="card-in-hand"></button>
+      <button class="card-in-hand"></button>
+    </div>
   </div>
 </template>
 
@@ -27,6 +52,11 @@ export default {
 </script>
 
 <style lang="scss">
+button {
+  outline: none;
+  // border: none;
+  box-shadow: 3px 5px 15px rgba(0, 0, 0, .5);
+}
 .main {
   position: fixed;
   left: 0;
@@ -36,15 +66,84 @@ export default {
   overflow: hidden;
   .player1 {
     position: fixed;
+    height: 20%;
     left: 50%;
     top: 0;
     transform: translate3d(-50%, 0, 0);
+    z-index: 100;
   }
   .player2 {
     position: fixed;
+    height: 20%;
     left: 50%;
     bottom: 0;
     transform: translate3d(-50%, 0, 0);
+    z-index: 100;
+  }
+  .end-round {
+    position: fixed;
+    right: 0;
+    top: 50%;
+    transform: translate3d(0, -50%, 0);
+    z-index: 100;
+  }
+  .ground {
+    position: fixed;
+    height: 70%;
+    width: 100%;
+    left: 0;
+    top: 15%;
+    z-index: 50;
+  }
+
+  .opponent-table {
+    display: flex;
+    justify-content: center;
+    height: 0;
+    .card-in-table {
+      top: 23vh;
+    }
+  }
+  .self-table {
+    display: flex;
+    justify-content: center;
+    height: 0;
+    .card-in-table {
+      top: 52vh;
+    }
+  }
+  .card-in-table {
+    position: relative;
+    width: 10%;
+    height: 25vh;
+    border-radius: 50%;
+    z-index: 100;
+    margin: 0 2px;
+  }
+
+  .opponent-hand {
+    display: flex;
+    justify-content: flex-start;
+    height: 0;
+    .card-in-hand {
+      top: 0;
+    }
+  }
+  .self-hand {
+    display: flex;
+    justify-content: flex-end;
+    transition: all 1s;
+    height: 0;
+    .card-in-hand {
+      top: 82vh;
+    }
+  }
+  .card-in-hand {
+    position: relative;
+    width: 5%;
+    height: 15vh;
+    z-index: 200;
+    margin: 1vh;
   }
 }
 </style>
